fc96a27f178f7a035316418ef9e581f444ca2380
M. A. Rahal <m.a.rahal@outlook.com>
2021-10-28 12:46:46 +0100

    added teacher's conflict resolution method - simplified code and improved comments - implemented the calculation of K (weight of conflict) - implemented normalization with K - values are now rounded before printing them (but stored with full precision)

--- exercice1.py
+++ exercice1.py
@@ -1,21 +1,28 @@
-from my_pyds import Masse, str_to_set, Sources
+from my_pyds import Mass, Sources
 
 def main():
-    omega = str_to_set('p c s')
+    # exercice 1
+    # on écrit Ω comme étant des mots séparés par des virgules
+    omega = 'p, c, s'
+
+    # on stockes nos sources d'information dans une classe appelée 'Sources'
     sources = Sources(omega)
-    sources.add({'pc' : 0.65, 's' : 0.24})
+    sources.add({'p,c' : 0.65, 's' : 0.24})
     sources.add({'s' : 0.48})
     sources.add({'p' : 1.0/3, 'c' : 1.0/3, 's' : 1.0/3})
-    sources.add({'pcs' : 1})
+    sources.add({'p,c,s' : 1})
 
+    # afficher les masses des sources
     print(sources)
 
-    alpha = 0.12
+    # affaiblir la source 1 (indice 0) avec un taux α = 0.12
     source_infiable = 0
-    print(f'--- après affaiblissement ------ α = {alpha} ---------------------\n')
+    alpha = 0.12
     sources.affaiblir(source_infiable, alpha)
+    print(f'--- après affaiblissement ------ α = {alpha} ---------------------\n')
     sources.show_source(source_infiable)
 
+    # combiner les sources (en ignorant celles avec m(Ω) = 1)
     print(f'--- combinaison des sources ---------------------------\n')
     m = sources.combine(show_steps = True)
     print(f'--- après combinaison ---------------------------\n')
--- my_pyds.py
+++ my_pyds.py
@@ -1,9 +1,5 @@
 from pyds import MassFunction
-
-# --- Decimal library is used to make calculations exact, see https://docs.python.org/3/library/decimal.html from more details ------
-from decimal import *
-getcontext().prec=10
-# -----------------------------------------------------------------------------------------------------------------------------------
+DECIMAL_PRECISTION = 5 # round values for before printing them on screen
 
 # this class is like MassFunction, but it automatically accounts for Ω  (descernment frame — "cadre de descernement" in french)
 class Mass(MassFunction):
@@ -13,15 +9,52 @@ class Mass(MassFunction):
         - omega : string with comma seperated names, e.g : "A1, A2, A3", or "car emissions, central heating"
         - source : a set containing all the sets and their massses e.g : {'a' : 0.1, 'b, c' : '0.2'}, or {'industrial cause, natural cause' : 0.5}
         """
+        super().__init__()
         if omega is None:
             raise Exception('must specify omega in mass definition')
+        # transform text input into sets
+        source = Mass.transform_source(source)
+        omega = str_to_set(omega)
+
         s = 0.0
         for k,v in source.items():
             s += v
         if not source.get(omega, False) :
-            source[omega] = float(1 - Decimal(s))
+            source[omega] = 1 - s
 
-        super().__init__(source)
+        for s in source:
+            self[s] = source[s]
+
+
+    def conflict(self, other):
+        """
+        calculated the conflict :
+        K = ∑ m1(B) * m2(C) : B ∩ C = ∅,  ∀B,C ⊂ Ω
+        """
+        k = 0
+        for b in self.all():
+            for c in other.all():
+                if len(b & c) == 0:
+                    k += self[b] * other[c]
+        return k
+
+    def my_combine_conjunctive(self, other, normalization = True):
+        res = Mass(self.combine_conjunctive(other, normalization = False), self.frame())
+        return res.normalize_with_K(self.conflict(other))
+
+    def normalize_with_K(self, K):
+        """
+        Sets the mass of ∅ to 0, and divides all others by 1 - K
+        """
+        # set mass(∅) = 0
+        if frozenset() in self:
+            del self[frozenset()]
+
+        # for all others, divide by 1 - K where K = conflict constant
+        factor = 1 / (1 - K)
+        for k, v in self.items():
+            self[k] = v * factor
+        return self
 
     @staticmethod
     def transform_source(source):
@@ -30,55 +63,66 @@ class Mass(MassFunction):
         """
         new_source = {}
         for k,v in source.items():
-            item_set = frozenset([name.strip() for name in k.split(',')])
-            new_source[item_set] = v
+            new_source[str_to_set(k)] = v
+        return new_source
 
-
-    # just like bel() but you pass a string (space separeted names)
     def bel_str(self, source):
+        """
+        just like bel() but you pass a string (space separeted names)
+        """
         return self.bel(str_to_set(source))
 
-    # just like pl() but you pass a string (space separeted names)
+
     def pl_str(self, source):
+        """
+        just like pl() but you pass a string (space separeted names)
+        """
         return self.pl(str_to_set(source))
 
-    # just like q() but you pass a string (space separeted names)
+
     def q_str(self, source):
+        """
+        just like q() but you pass a string (space separeted names)
+        """
         return self.q(str_to_set(source))
 
     def show_beleif(self):
         i = 1
         for ensemble, bel in self.bel().items():
-            print(f'{i}- {set_to_str(ensemble)} : {bel}'); i += 1
+            print(f'{i}- {set_to_str(ensemble)} : {round(bel,DECIMAL_PRECISTION)}'); i += 1
 
     def __str__(self):
         text = ''
         for k,v in sorted(self.items(), key = lambda x : len(x[0])):
             if v > 0.0 or k == self.frame():
-                text += f'{set_to_str(k)} : {v}\n'
+                text += f'{set_to_str(k)} : {round(v,DECIMAL_PRECISTION)}\n'
         return text
 
 
-# use this class to combine multiple sources (having the same 'omega')
 class Sources(list):
     def __init__(self, omega):
+        """
+        use this class to combine multiple sources (having the same 'omega')
+        """
         super().__init__()
-        self.omega = omega
+        self.omega = str_to_set(omega)
         self.sources_affaiblis = set()
 
     def add(self, source):
         self.append(Mass(source, self.omega))
 
-    # affaiblir une source avec un taux alpha d'affaiblissement
     def affaiblir(self, source_index, alpha):
+        """
+        affaiblir une source avec un taux alpha d'affaiblissement
+        """
         assert 0 <= alpha <= 1, 'alpha must be in range [0,1]'
         source = self[source_index]
         for s in source.keys():
             if s != self.omega: # affaiblire la masse des elements autre que omega
-                source[s] = Decimal(1 - alpha)*Decimal(source[s])
+                source[s] = (1 - alpha)*source[s]
             else: # augmenter la masse de omega
-                source[s] = Decimal(1 - alpha)*Decimal(source[s]) + Decimal(alpha)
-            source[s] = float(source[s])
+                source[s] = (1 - alpha)*(source[s]) + alpha
+            source[s] = source[s]
         
         # ajouter la source au sources affaiblis (affin de bien combiner par la suite)
         self.sources_affaiblis.add(source_index)
@@ -86,7 +130,7 @@ class Sources(list):
     def __str__(self):
         text = ''
         for i, s in enumerate(self):
-            text += f'resource {i+1} :\n'
+            text += f'masses de la source {i+1} :\n'
             text += str(s) + '\n'
         return text
 
@@ -113,18 +157,31 @@ class Sources(list):
         count = 1
         while i < len(sources):
             s2 = sources[i]; i += 1
-            res = s1.combine_conjunctive(s2, normalization=True)
+            res = s1.my_combine_conjunctive(s2, normalization=True)
             if show_steps:
                 print(f'combinaison {count} :'); count += 1
-                print('k =', s1.conflict(s2))
+                k = s1.conflict(s2)
+                print('k =', round(k, DECIMAL_PRECISTION), end = ', ')
+                print('1/(1-k) =', round(1/(1-k), DECIMAL_PRECISTION))
                 print(Mass(res, self.omega))
 
             s1 = res
         return Mass(s1, self.omega)
 
 
+
+def str_to_set(text):
+    """
+    transforms comma separated string into a set
+    'name1, name2, name3' -> {'name1', 'name2', 'name3'}
+    """
+    if isinstance(text, str):
+        return frozenset([name.strip() for name in text.split(',')])
+    elif isinstance(text, set) or isinstance(text, frozenset): # if its already a set, return it (might happen!)
+        return text
+    else:
+        raise Exception(f'the input "{text}" was supposed to be a string (or maybe a set), introduced to function set_to_str')
+
 def set_to_str(s):
     return "{" + ', '.join([str(x) for x in s]) + "}"
 
-def str_to_set(text):
-    return frozenset(text.split())
